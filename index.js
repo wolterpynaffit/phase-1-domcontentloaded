d686683d37806bd593b1b4eb6d030421d8ae21d2
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-07-25 22:26:23 -0400

    finsshed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const currentText = document.querySelector('#text')
+  currentText.textContent = "This is really cool!"
+});
